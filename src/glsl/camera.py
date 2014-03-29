c33e775ad6c948eb0250a1d6aa230a52c7fa3abc
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-03-29 14:45:02 +0100

    Fixes some mistakes in GLSL shaders

--- src/glsl/camera.py
+++ src/glsl/camera.py
@@ -6,10 +6,10 @@ class Camera:
 
         position = vec3
         direction = vec3
-        fill_of_view = float
+        field_of_view = float
     """
 
     def __init__(self):
         self.position = [0.0, -10.0, 0.0]
         self.direction = [0.0, 1.0, 0.0]
-        self.fill_of_view = math.pi * 0.5
+        self.field_of_view = math.pi * 0.5
