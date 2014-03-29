6cead343a6d763a4180c79c42dda9ecaa6a395ea
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-03-29 13:11:14 +0100

    Implements Sphere class

---
+++ src/glsl/primitives.py
@@ -0,0 +1,10 @@
+
+class Sphere:
+    """
+        center = vec3
+        radius = float
+    """
+
+    def __init__(self, center = [0.0, 0.0, 0.0], radius = 1.0):
+        self.center = center
+        self.radius = radius
