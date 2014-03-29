0e895b79665fe0eda3ba82233a62508dde77d0c0
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-03-29 13:57:53 +0100

    Implements glsl.utils.code_vec()

--- src/glsl/library.py
+++ src/glsl/library.py
@@ -1,4 +1,6 @@
 
+import utils
+
 # ------------------------------------------------------------------------------ GLSL HEADER
 
 glsl_header = """
@@ -62,19 +64,40 @@ object_sphere(vec4 sphere)
 
 # ------------------------------------------------------------------------------ GLSL MAIN
 
-glsl_main = """
+glsl_ray_launch = """
 
-void
-main()
+vec3
+ray_launch(vec3 origin, vec3 dir)
 {
-    ray_origin = vec3(0.0);
-    ray_dir = vec(0.0, 1.0, 0.0);
+    ray_origin = origin;
+    ray_dir = dir;
     ray_intersection_dist = MATH_FAR;
 
-    launch_ray();
+    return vec3(0.0);
 }
 
 """
+
+
+# ------------------------------------------------------------------------------ GLSL MAIN
+
+def glsl_main(scene):
+    code_tmplt = """
+
+void
+main()
+{{
+    vec3 camera_origin = {camera_origin};
+    vec3 dir = vec(0.0, 1.0, 0.0);
+
+    gl_FragColor = vec4(ray_launch(origin, dir), 1.0);
+}}
+
+"""
+
+    camera_origin = utils.code_vec(scene.camera.position)
+
+    return code_tmplt.format(camera_origin=camera_origin)
 
 
 # ------------------------------------------------------------------------------ MAIN
@@ -85,6 +108,6 @@ def main(scene):
     glsl_code += glsl_header
     glsl_code += glsl_global_ray
     glsl_code += glsl_sphere_code
-    glsl_code += glsl_main
+    glsl_code += glsl_main(scene)
 
     return glsl_code
