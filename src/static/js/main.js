75f2b71e8bbe7f56278d0105fc75c2bad1a637a8
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2014-03-29 13:20:41 +0100

    Added "apiCall" function

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,3 +1,14 @@
+function apiCall(path, method, params, callback) {
+    $.ajax({
+      url: path,
+      type: method,
+      async: true,
+      dataType: "json",
+      data: JSON.stringify(params),
+      contentType: 'application/json;charset=UTF-8',
+      success: callback
+    });
+}
 
 function main()
 {
