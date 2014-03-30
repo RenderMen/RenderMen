dade6d9477e86e5f06ecd061ddd6afe764553eb1
Thierry CANTENOT <thierry.cantenot@insa-lyon.fr>
2014-03-30 08:06:13 +0200

    Added transparent material

--- src/glsl/material.py
+++ src/glsl/material.py
@@ -40,10 +40,100 @@ material_glossy(vec3 albedo)
     ray_continue(new_ray_dir);
 }
 
-void
-material_translucent(vec3 albedo)
-{
+struct
+Fresnel {
+    float reflectionCoefficient;
+    float transmissionCoefficient;
+};
 
+Fresnel
+calculateFresnel(vec3 normal, vec3 incident, float incidentIOR, float transmittedIOR) {
+
+    Fresnel fresnel;
+
+    incident = normalize(incident);
+    normal = normalize(normal);
+
+    if (incidentIOR <= 0.0 && transmittedIOR <= 0.0 ) {
+        fresnel.reflectionCoefficient = 1.0;
+        fresnel.transmissionCoefficient = 0.0;
+        return fresnel;
+    } else{
+        float cosThetaI = abs(dot(normal, incident));
+        float sinIncidence = sqrt(1.0-pow(cosThetaI,2.0));
+        float cosThetaT = sqrt(1.0-pow(((incidentIOR/transmittedIOR)*sinIncidence),2.0));
+        float RsP = pow( (incidentIOR * cosThetaI - transmittedIOR * cosThetaT) / (incidentIOR * cosThetaI + transmittedIOR * cosThetaT) , 2.0);
+        float RpP = pow( (incidentIOR * cosThetaT - transmittedIOR * cosThetaI) / (incidentIOR * cosThetaT + transmittedIOR * cosThetaI) , 2.0);
+        fresnel.reflectionCoefficient = (RsP + RpP) / 2.0;
+        fresnel.transmissionCoefficient = 1.0 - fresnel.reflectionCoefficient;
+        return fresnel;
+    }
+}
+
+vec3
+calculateTransmissionDirection(vec3 normal, vec3 incident, float incidentIOR, float transmittedIOR) {
+
+    float dotValue = dot(normal, -incident);
+
+    float eta = incidentIOR / transmittedIOR;//eta
+
+    float k = 1.0 - pow(eta, 2.0) * (1.0 - pow(dotValue, 2.0));
+
+    if(k < 0.0) {
+        return vec3(0.0); //calculateReflectionDirection(normal,incident);
+    }
+    else {
+        return normalize((incident) * eta + normal * (eta * dotValue - sqrt(k)));
+    }
+}
+
+
+void
+material_transparent_front(vec3 albedo, float refractFactor)
+{
+    Fresnel fresnel = calculateFresnel(attr_normal, ray_dir, 1.0 - refractFactor, refractFactor);
+
+    float russian_roulette = noise1D(attr_normal.x); //snoise(attr_normal);
+
+    if(russian_roulette < fresnel.transmissionCoefficient)
+    {
+        ray_color = albedo;
+        vec3 refl_ray_dir = reflect(ray_dir, attr_normal); //ray_dir - attr_normal * 2 * attr_normal * ray_dir;
+        ray_continue(refl_ray_dir);
+        return;
+    }
+    else
+    {
+        //vec3 new_ray_dir = calculateTransmissionDirection(attr_normal, ray_dir, 0.2, 0.8);//1.0 - refractFactor, refractFactor);
+        vec3 new_ray_dir = ray_dir;
+        ray_color = albedo;
+        ray_continue(new_ray_dir);
+        return;
+    }
+}
+
+void
+material_transparent_back(vec3 albedo, float refractFactor)
+{
+    // TODO
+    ray_color = albedo;
+    ray_continue(ray_dir);
+}
+
+void
+material_transparent(vec3 albedo, float refractFactor)
+{
+    material_transparent_front(albedo, refractFactor);
+    return;
+
+    if(dot(attr_normal, ray_dir) < 0.0) // Ray from outside
+    {
+        material_transparent_front(albedo, refractFactor);
+    }
+    else // Ray from inside
+    {
+        material_transparent_back(albedo, refractFactor);
+    }
 }
 
 """
@@ -111,3 +201,20 @@ class Glossy(Abstract):
             albedo=utils.code_vec(self.albedo)
         )
 
+
+# ------------------------------------------------------------------------------ TRANSPARENT MATERIAL
+
+class Transparent(Abstract):
+
+    albedo = mongoengine.ListField(mongoengine.FloatField(), default=lambda : [0.8, 0.8, 0.8])
+    refract_factor = mongoengine.FloatField(0.6)
+
+    def code(self):
+        code_tmplt = "material_transparent({albedo}, {refract_factor});"
+
+        print self.refract_factor
+
+        return code_tmplt.format(
+            albedo=utils.code_vec(self.albedo),
+            refract_factor=self.refract_factor,
+        )
--- src/glsl/scene.py
+++ src/glsl/scene.py
@@ -138,8 +138,8 @@ def boiler_scene(user, title, description):
     s.add(primitives.Sphere(center=[0.0, 0.0, 10.0], radius=3.0, material=material.Emit(color=[12.0, 12.0, 12.0])))
     s.add(primitives.Sphere(center=[0.0, 3.0, 1.0], radius=1.0, material=material.Mirror()))
     s.add(primitives.Sphere(center=[-4.0, 3.0, 1.0], radius=1.0))#, material=material.Glossy()))
-    s.add(primitives.Sphere(center=[0.0, 3.0, 1.0], radius=1.0))#, material=material.Mirror()))
-    s.add(primitives.Sphere(center=[-2.5, 1.0, 1.0], radius=1.0))#, material=material.Glossy()))
+    s.add(primitives.Sphere(center=[-1.0, -1.0, 1.0], radius=1.0))#, material=material.Mirror()))
+    s.add(primitives.Sphere(center=[-2.5, 1.0, 2.5], radius=2.0, albedo=[1.0, 1.0, 1.0], material=material.Transparent(refract_factor=0.94)))
     s.add(primitives.Cube(cubeMin=[2.0, -2.0, 1.0], cubeMax=[5.5, 2.0, 3.0], material=material.Mirror()))
     s.add(primitives.Triangle(A=[-6.5, 5.0, 0.0], B=[-2.0, 6.5, 0.0], C=[-6.5, 5.0, 5.0]
         , material = material.Mirror()))
