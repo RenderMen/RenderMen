9616f258e47da1d445bff3ab84f9f63ba21c84f6
Thierry CANTENOT <thierry.cantenot@insa-lyon.fr>
2014-04-06 18:02:52 +0200

    Added insert method to MeshOctree

--- src/js/MeshOctree.js
+++ src/js/MeshOctree.js
@@ -1,3 +1,8 @@
+// Require MeshOctreeNode.js
+// Require Vector3.js
+// Require Box.js
+// Require utils.js
+
 // Mesh Octree class
 function MeshOctree(mesh)
 {
@@ -7,14 +12,14 @@ function MeshOctree(mesh)
     // Root of the octree
     this.root = new MeshOctreeNode(this);
 
-    // Width of the octree
-    this.width = 0;
+    // Length on x-axis of the octree
+    this.dx = 0;
 
-    // Height of the octree
-    this.height = 0;
+    // Length on y-axis of the octree
+    this.dy = 0;
 
-    // Depth of the octree
-    this.depth = 0;
+    // Length on z-axis of the octree
+    this.dz = 0;
 
     // Position of the octree
     this.pos = new Vector3(0.0, 0.0, 0.0);
@@ -39,6 +44,7 @@ MeshOctree.prototype.init = function()
 
     var vertices = vertexAttr.attributes;
 
+    // Retrieve Octree dimensions
     for(var i = 0; i < vertices.length - 2; i += 3)
     {
         min.x = min(vertices[i], min.x);
@@ -51,34 +57,142 @@ MeshOctree.prototype.init = function()
         max.z = max(vertices[i+2], max.z);
     }
 
-    this.width  = max.x - min.x;
-    this.height = max.y - min.y;
-    this.depth  = max.z - min.z;
+    this.dx = max.x - min.x;
+    this.dy = max.y - min.y;
+    this.dz = max.z - min.z;
 
-    this.pos = min;
+    //TODO
+    // Ruond to upper power of two
+
+    this.pos = min; // Front lower left vertice of the octree
 
     console.log("Octree min : " + min.toString());
     console.log("Octree max : " + max.toString());
 
+    // Insert the mesh's triangle into the octree
+    var bounding = new Box();
     for(var i = 0; i < vertices.length - 8; i += 9)
     {
-        min.x = min(vertices[i], vertices[i+3], vertices[i+6]);
-        max.x = max(vertices[i], vertices[i+3], vertices[i+6]);
+        bounding.min.x = min(vertices[i], vertices[i+3], vertices[i+6]);
+        bounding.max.x = max(vertices[i], vertices[i+3], vertices[i+6]);
 
-        min.y = min(vertices[i+1], vertices[i+4], vertices[i+7]);
-        max.y = max(vertices[i+1], vertices[i+4], vertices[i+7]);
+        bounding.min.y = min(vertices[i+1], vertices[i+4], vertices[i+7]);
+        bounding.max.y = max(vertices[i+1], vertices[i+4], vertices[i+7]);
 
-        min.z = min(vertices[i+2], vertices[i+5], vertices[i+8]);
-        max.z = max(vertices[i+2], vertices[i+5], vertices[i+8]);
+        bounding.min.z = min(vertices[i+2], vertices[i+5], vertices[i+8]);
+        bounding.max.z = max(vertices[i+2], vertices[i+5], vertices[i+8]);
 
-
+        this.insert(i / 9, bounding);
     }
 }
 
 /*
  * Insert a triangle in the octree
+ *
+ * @param indice : indice of the triangle to insert
+ * @param bounding : bounding box of the triangle
  */
-MeshOctree.prototype.insert = function(p1, p2, p3)
+MeshOctree.prototype.insert = function(indice, bounding)
 {
+    assert(this.root != null, "The root of the octree must not be null");
 
+    var mid = new Vector3(
+        pos.x + this.dx / 2.0;
+        pos.y + this.dy / 2.0;
+        pos.z + this.dz / 2.0;
+    )
+
+    var half_dx = this.dx / 2;
+    var half_dy = this.dy / 2;
+    var half_dz = this.dz / 2;
+
+    var b = bounding;
+    var inserted = false;
+    var node = this.root;
+
+    var offset = new Vector3();
+
+   while(!inserted)
+    {
+        half_dx /= 2.0;
+        half_dy /= 2.0;
+        half_dz /= 2.0;
+
+        if((b.min.x < mid.x) && (b.max.x > mid.x) ||
+           (b.min.y < mid.y) && (b.min.y < mid.y) ||
+           (b.min.z < mid.z) && (b.min.z < mid.z)
+        {
+            node.insert(indice);
+            inserted = true;
+            continue;
+        }
+
+        if(node.children.length == 0)
+        {
+            node.split();
+        }
+
+        if(b.max.x < mid.x)
+        {
+            if(b.max.y < mid.y)
+            {
+                if(b.max.z < mid.z)
+                {
+                    node = node.children[0];
+                    offset.set(-half_dx, -half_dy, -half_dz);
+                }
+                else
+                {
+                    node = node.children[1];
+                    offset.set(-half_dx, -half_dy, +half_dz);
+                }
+            }
+            else
+            {
+                if(b.max.z < mid.z)
+                {
+                    node = node.children[2];
+                    offset.set(-half_dx, +half_dy, -half_dz);
+                }
+                else
+                {
+                    node = node.children[3];
+                    offset.set(-half_dx, +half_dy, +half_dz);
+                }
+            }
+        }
+        else
+        {
+            if(b.max.y < mid.y)
+            {
+                if(b.max.z < mid.z)
+                {
+                    node = node.children[4];
+                    offset.set(+half_dx, -half_dy, -half_dz);
+                }
+                else
+                {
+                    node = node.children[5];
+                    offset.set(+half_dx, -half_dy, +half_dz);
+                }
+            }
+            else
+            {
+                if(b.max.z < mid.z)
+                {
+                    node = node.children[6];
+                    offset.set(+half_dx, +half_dy, -half_dz);
+                }
+                else
+                {
+                    node = node.children[7];
+                    offset.set(+half_dx, +half_dy, +half_dz);
+                }
+            }
+        }
+
+        mid.x += offset.x;
+        mid.y += offset.y;
+        mid.z += offset.z;
+    }
 }
