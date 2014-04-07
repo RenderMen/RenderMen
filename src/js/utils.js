efbf6d98c92fb2e5bd71cdcbb4b7e6f14de354dc
Thierry CANTENOT <thierry.cantenot@insa-lyon.fr>
2014-04-08 00:34:48 +0200

    Added next_power_of_2 function

--- src/js/utils.js
+++ src/js/utils.js
@@ -16,3 +16,16 @@ function min3(x, y, z)
 {
     return Math.min(x, Math.min(y, z));
 }
+
+// Gets the least power of 2 greater than or equal to x
+// (32 bits version)
+function next_power_of_2(x)
+{
+    x = x - 1;
+    x |= (x >> 1);
+    x |= (x >> 2);
+    x |= (x >> 4);
+    x |= (x >> 8);
+    x |= (x >> 16);
+    return x + 1;
+}
